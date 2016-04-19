42ecb71575e96518c18812b3b33ce17f32beb925
Antero Oikkonen <antero.oikkonen@gmail.com>
2016-04-19 16:19:43 +0300

    KauppaTest lisää testejä

--- Verkkokauppa4/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa4/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -45,6 +45,37 @@ public class KauppaTest {
     }
 
     @Test
+    public void ostoksenKaksiTavaraaPaatyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(1, "juusto", 10));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2); 
+        k.tilimaksu("pekka", "12347");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12347"), eq("33333-44455"), eq(15));
+    // t
+    }
+    
+    
+    @Test
     public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaan() {
         // luodaan ensin mock-oliot
         Pankki pankki = mock(Pankki.class);
